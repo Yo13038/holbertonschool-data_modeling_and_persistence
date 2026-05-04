6acf13fb60cdbb53050678ead68bb9c58d4191c1
Yo13038 <13038@holbertonstudents.com>
2026-05-04 13:24:14 +0200

    new correct version of the structured_output.py with variable

--- python_fundamentals/hello_world/structured_output.py
+++ python_fundamentals/hello_world/structured_output.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+langage = "Python"
+version = 3
+pi = 3.14
 
-print("Language: Python")
-print("Version: 3")
-print("Pi approx: 3.14")
-print("Computation valid: True")
+print(f"Language: {langage} ")
+print(f"Version: {version}")
+print(f"Pi approx: {pi}")
+
+if langage == "Python" and version == 3 and pi == 3.14:
+    print("Computation valid: True")
