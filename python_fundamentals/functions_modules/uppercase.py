21e1c0984b88add4acb144f2e0ce96825e37c051
Yo13038 <13038@holbertonstudents.com>
2026-05-05 10:27:48 +0200

    function that print a string in uppercase

---
+++ python_fundamentals/functions_modules/uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+def uppercase(str):
+
+    result = ""
+    for i in str:
+        ascii_val = ord(i)
+        if 97 <= ascii_val <= 122:
+            result += chr(ascii_val - 32)
+        else:
+            result += i
+    print(result)
+
+
+uppercase("Holberton")
