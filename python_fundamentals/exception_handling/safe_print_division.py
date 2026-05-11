bb193994512ca69530fce8ffdd20351b27c7a356
Yo13038 <13038@holbertonstudents.com>
2026-05-11 16:51:56 +0200

    function that divide 2 integers

---
+++ python_fundamentals/exception_handling/safe_print_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+modul that handle a divide operation
+"""
+def safe_print_division(a, b):
+    """
+    function that handle the divide by zero erreur of a division
+    """
+    result = None
+    try:
+        result = a / b
+    except (ZeroDivisionError, TypeError):
+        pass
+    finally:
+        print("Inside result: {}".format(result))
+
+    return result
