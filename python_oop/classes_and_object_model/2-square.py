cc85e9d10dc8ce788b60ab2323c4543d2df93917
Yo13038 <13038@holbertonstudents.com>
2026-05-12 14:08:11 +0200

    class who check if the object is valit

---
+++ python_oop/classes_and_object_model/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+add validation to the size of a square
+"""
+
+
+class Square():
+    """
+    class square difine by his size
+    """
+    def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
