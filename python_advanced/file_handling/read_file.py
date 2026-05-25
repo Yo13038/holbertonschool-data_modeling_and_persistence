78faa98461b3796ae2e09b4466d8b298fe8a7480
Yo13038 <13038@holbertonstudents.com>
2026-05-25 14:22:28 +0200

    change the end of line sequence and add Module description

--- python_advanced/file_handling/read_file.py
+++ python_advanced/file_handling/read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-
+"""Modul for read_file
+"""
 def read_file(filename=""):
-
+    """read file"""
     with open(filename, encoding="utf-8") as file_name:
 
         print(file_name.read(), end="")
