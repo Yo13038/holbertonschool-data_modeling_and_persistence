e8e0f2a43008c6f609c9bc3f1c88026e5aed8784
Yo13038 <13038@holbertonstudents.com>
2026-06-03 16:38:44 +0200

    Retrieve the list of courses that have more enrollments than the average number of enrollments per course

---
+++ sql_join/6-courses-above-average.sql
@@ -0,0 +1,15 @@
+SELECT courses.title AS course_title
+FROM courses
+INNER JOIN enrollments 
+ON courses.id = enrollments.course_id
+GROUP BY courses.id, courses.title
+HAVING 
+    COUNT(enrollments.student_id) > (
+        SELECT AVG(student_count)
+        FROM (
+            SELECT COUNT(student_id) AS student_count
+            FROM enrollments
+            GROUP BY course_id
+        )
+    )
+ORDER BY course_title ASC;
