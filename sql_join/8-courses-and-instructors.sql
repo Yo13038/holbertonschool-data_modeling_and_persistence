b0b8bfd9f0378d2f9fdb7f0f4323d527c16162d4
Yo13038 <13038@holbertonstudents.com>
2026-06-04 11:18:49 +0200

    scenario 1 of task 8

---
+++ sql_join/8-courses-and-instructors.sql
@@ -0,0 +1,5 @@
+SELECT courses.title AS course_title, instructors.name AS instructor_name
+FROM courses
+INNER JOIN instructors
+ON courses.instructor_id = instructors.id
+ORDER BY course_title ASC;
