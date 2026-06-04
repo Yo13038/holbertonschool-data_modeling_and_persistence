e8ff9c4df715662109f8775d6a6d3c236fe329fc
Yo13038 <13038@holbertonstudents.com>
2026-06-04 10:45:37 +0200

    Objective Retrieve a list of all courses along with the number of students enrolled in each course

---
+++ sql_join/7-course-enrollment-count.sql
@@ -0,0 +1,8 @@
+SELECT courses.title AS course_title,
+COUNT(enrollments.student_id) AS enrollments_count
+FROM courses
+LEFT JOIN enrollments
+ON courses.id = enrollments.course_id
+GROUP BY courses.id, courses.title
+ORDER BY enrollments_count DESC,
+course_title ASC;
